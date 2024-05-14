7b908b1c9f5c439c660d78f7407d59df894cfe56
unknown <ben.ruelas@tcu.edu>
2024-05-13 20:33:14 -0500

    Finished mobile share button

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const shareWidget = document.getElementById("share-widget");
 const shareBtn = document.getElementById("share-btn");
+const shareBtnMobile = document.getElementById("share-btn-mobile");
+const author = document.getElementById("author");
+const authorAndShare = document.getElementById("author-and-share");
+const shareWidgetMobile = document.getElementById("share-widget-mobile");
+const card = document.getElementById("card");
+const cardContent = document.getElementById("card-content");
 
 let flag = false;
 shareBtn.addEventListener("click", (e) => {
-  if (flag) {
-    shareWidget.className = "share-widget";
+  if (window.innerWidth > 905) {
+    if (flag) {
+      shareWidget.className = "share-widget";
+    } else {
+      shareWidget.className = "hide";
+    }
+    flag = !flag;
   } else {
-    shareWidget.className = "hide";
+    if (flag) {
+      author.className = "author";
+      shareWidgetMobile.className = "hide";
+    } else {
+      author.className = "hide";
+      authorAndShare.className = "hide";
+      shareWidgetMobile.className = "share-widget-mobile";
+      shareWidgetMobile.style.width = card.style.width;
+      shareBtn.className = "hide";
+    }
+    flag = !flag;
+  }
+});
+
+shareBtnMobile.addEventListener("click", (e) => {
+  if (flag) {
+    author.className = "author";
+    authorAndShare.className = "author-and-share";
+    shareWidgetMobile.className = "hide";
+    shareBtn.className = "icon icon-share";
+  } else {
+    shareWidgetMobile.className = "share-widget-mobile";
+    shareWidgetMobile.style.width = card.style.width;
+
+    cardContent.style.justifyContent = "center";
   }
   flag = !flag;
 });
